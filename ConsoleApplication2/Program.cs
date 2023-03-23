09e6b86202ed189c829333e61d9260c182666460
MishaGoroshko <mihagoroh@gmail.com>
2023-03-23 15:37:59 +0100

    Initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10;
+
+            Console.WriteLine(x);
+        }
+    }
+}
