780be6a0d026a404c0b59ac8b98b0bbb2b42b784
MishaGoroshko <mihagoroh@gmail.com>
2023-03-24 11:03:09 +0100

    Parse,TryParse, out

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleApplication2
 {
@@ -21,9 +22,50 @@ namespace ConsoleApplication2
                 NumberDecimalSeparator = "."
             };
 
-            string num = "1.9";
+            string num = "1.1";
 
             Console.WriteLine(Convert.ToDouble(num, numberFormatInfo));
+
+            string num2 = "22";
+
+            Console.WriteLine(200 + int.Parse(num2));
+
+            string num3 = "33,33";
+
+            Console.WriteLine(double.Parse(num3));
+
+            string num4 = "44.44";
+
+            Console.WriteLine(double.Parse(num4, numberFormatInfo));
+
+            string num5 = "5qwerty";
+
+            try
+            {
+                Console.WriteLine(int.Parse(num5));
+                Console.WriteLine("Parse is successful");
+            }
+            catch (Exception e)
+            {
+                // Console.WriteLine(e.Message);
+                // throw;
+                Console.WriteLine($"{num5} ---- it is not possible to parse");
+            }
+
+            string mnum6 = "6";
+
+            int x;
+            bool result = int.TryParse(mnum6, out x);
+            // bool result = int.TryParse(mnum6, out int x);
+
+            if (result)
+            {
+                Console.WriteLine($"Parse is successful, number = {x}");
+            }
+            else
+            {
+                Console.WriteLine($"{x} ---- it is not possible to parse");
+            }
         }
     }
 }
