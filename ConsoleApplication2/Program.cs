01c12153ad869e8a6f14ad9fe65ca9c8df91882e
MishaGoroshko <mihagoroh@gmail.com>
2023-03-24 13:11:53 +0100

    (double)a / b

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,64 +8,50 @@ namespace ConsoleApplication2
     {
         public static void Main(string[] args)
         {
-            // string data = Console.ReadLine();
+            // int a = 5;
+            // int b = 2;
+
+            // double a = 5;
+            // int b = 2;
+
+            // double res = a / b;
+
+            // int a = 5;
+            // int b = 2;
             //
-            // int res = 100 + Convert.ToInt32(data);
+            // double res = (double)(a + b) / 2;
             //
             // Console.WriteLine(res);
+            //
+            // int num1 = int.Parse(Console.ReadLine());
+            // int num2 = int.Parse(Console.ReadLine());
+            // int num3 = int.Parse(Console.ReadLine());
+            //
+            // Console.WriteLine("amount of numbers " + (num1 + num2 + num3));
+            // Console.WriteLine("multiply of numbers " + (num1 * num2 * num3));
+            Console.WriteLine("choose the number currency for purchase:\n" +
+                              "dollar -- 1\n" +
+                              "euro ---- 2\n" +
+                              "zloty --- 3");
+            int currency1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"currency for purchase {currency1}");
 
-            // Console.WriteLine($"you have typed {data}");
-            // Console.WriteLine("you have typed " + data);
+            Console.WriteLine("choose the number currency for sell:\n" +
+                              "dollar -- 1\n" +
+                              "euro ---- 2\n" +
+                              "zloty --- 3");
+            int currency2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"currency for sell {currency2}");
 
-            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
-            {
-                NumberDecimalSeparator = "."
-            };
+            Console.WriteLine("type amount");
+            double amount = double.Parse(Console.ReadLine());
 
-            string num = "1.1";
+            double result;
+            if (currency1 == 1 && currency2 == 2) result = amount * 1.1;
+            else if (currency1 == 1 && currency2 == 3) result = amount * 4.4;
+            else result = amount * 4.9;
 
-            Console.WriteLine(Convert.ToDouble(num, numberFormatInfo));
-
-            string num2 = "22";
-
-            Console.WriteLine(200 + int.Parse(num2));
-
-            string num3 = "33,33";
-
-            Console.WriteLine(double.Parse(num3));
-
-            string num4 = "44.44";
-
-            Console.WriteLine(double.Parse(num4, numberFormatInfo));
-
-            string num5 = "5qwerty";
-
-            try
-            {
-                Console.WriteLine(int.Parse(num5));
-                Console.WriteLine("Parse is successful");
-            }
-            catch (Exception e)
-            {
-                // Console.WriteLine(e.Message);
-                // throw;
-                Console.WriteLine($"{num5} ---- it is not possible to parse");
-            }
-
-            string mnum6 = "6";
-
-            int x;
-            bool result = int.TryParse(mnum6, out x);
-            // bool result = int.TryParse(mnum6, out int x);
-
-            if (result)
-            {
-                Console.WriteLine($"Parse is successful, number = {x}");
-            }
-            else
-            {
-                Console.WriteLine($"{x} ---- it is not possible to parse");
-            }
+            Console.WriteLine($"currency exchange result -- {result}");
         }
     }
 }
