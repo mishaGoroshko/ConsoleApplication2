e71d4632a1216b640316b38f66fecb93d9542608
MishaGoroshko <mihagoroh@gmail.com>
2023-03-27 22:34:36 +0200

    console

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,37 +8,19 @@ namespace ConsoleApplication2
     {
         public static void Main(string[] args)
         {
-            // int name = Convert.ToInt32(Console.ReadLine());
-            //
-            // while (name-- > 1)
-            // {
-            //     Console.WriteLine(name);
-            // }
-            //
-            // Random rand = new Random();
+            Console.SetCursorPosition(10, 5);
+
+            Console.WindowHeight = 20;
+            Console.WindowWidth = 50;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();// console will set completely gray
+
+            Console.WriteLine($"lorem lorem\nlorem lorem\nlorem lorem]\b \b");
 
 
-            // while (true)
-            // {
-            //     int num = rand.Next(0, 5);
-            //     Console.WriteLine(num);
-            //     Console.ReadKey();
-            // }
-
-            for (int i = 0; i < 5; i++)
-            {
-                Random rand2 = new Random();
-                Console.WriteLine(rand2.Next(0, 5));// all the same results
-            }
-
-            Console.WriteLine("----------------------");
-
-            Random rand3 = new Random();
-
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(rand3.Next(0, 5));//all different results
-            }
+            Console.ReadKey();
         }
     }
 }
