9d09707be823c1515a1c3b3e88f2d1068bf606c4
MishaGoroshko <mihagoroh@gmail.com>
2023-03-27 22:07:59 +0200

    Random

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,11 +8,36 @@ namespace ConsoleApplication2
     {
         public static void Main(string[] args)
         {
-            int name = Convert.ToInt32(Console.ReadLine());
+            // int name = Convert.ToInt32(Console.ReadLine());
+            //
+            // while (name-- > 1)
+            // {
+            //     Console.WriteLine(name);
+            // }
+            //
+            // Random rand = new Random();
 
-            while (name-- > 1)
+
+            // while (true)
+            // {
+            //     int num = rand.Next(0, 5);
+            //     Console.WriteLine(num);
+            //     Console.ReadKey();
+            // }
+
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(name);
+                Random rand2 = new Random();
+                Console.WriteLine(rand2.Next(0, 5));// all the same results
+            }
+
+            Console.WriteLine("----------------------");
+
+            Random rand3 = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(rand3.Next(0, 5));//all different results
             }
         }
     }
