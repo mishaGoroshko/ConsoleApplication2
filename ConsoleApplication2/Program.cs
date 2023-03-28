c0057b217e4703afddddc2dcb1631416c60dc566
MishaGoroshko <mihagoroh@gmail.com>
2023-03-28 09:55:25 +0200

    array

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace ConsoleApplication2
@@ -8,19 +9,48 @@ namespace ConsoleApplication2
     {
         public static void Main(string[] args)
         {
-            Console.SetCursorPosition(10, 5);
+            int[] arr1 = { 1, 2, 3, int.MaxValue, int.MinValue };
+            int[] arr2 = new int[4] { 1, 2, 3, 4 };
+            int[] arr = new int[3];
 
-            Console.WindowHeight = 20;
-            Console.WindowWidth = 50;
+            Random rand = new Random();
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.Clear();// console will set completely gray
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 11);
+                Console.Write(arr[i] + " ");
+            }
 
-            Console.WriteLine($"lorem lorem\nlorem lorem\nlorem lorem]\b \b");
+            Console.WriteLine("\n--------------------");
 
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
 
-            Console.ReadKey();
+            Console.WriteLine("\n---------sum-----------");
+
+            int[] arr3 = { 1, 3, 4 };
+            int sum = 0;
+            for (int i = 0; i < arr3.Length; i++)
+            {
+                sum += arr3[i];
+            }
+
+            Console.WriteLine(sum);
+
+            Console.WriteLine("-----maximum number------");
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < arr3.Length; i++)
+            {
+                if (maxNumber < arr3[i])
+                {
+                    maxNumber = arr3[i];
+                }  
+            }
+
+            Console.WriteLine(maxNumber);
         }
     }
 }
