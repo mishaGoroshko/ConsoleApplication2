24dca1721c4e6dcad4e6e5e364e3a2cffc168b65
MishaGoroshko <mihagoroh@gmail.com>
2023-03-28 15:52:21 +0200

    function, optional parameters

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -7,31 +7,13 @@ namespace ConsoleApplication2
     {
         public static void Main(string[] args)
         {
-            int[] arr = new int[4] { 1, 2, 3, 4 };
-            int[] extraArr = new int[5];
+            Console.WriteLine(Sum(2, 4));
+            Console.WriteLine(Sum(2, word: "abc"));
+        }
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                extraArr[i] = arr[i];
-            }
-
-            arr = extraArr;
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-
-            Console.WriteLine("------------------");
-
-            int[] arr2 = new int[4] { 1, 2, 3, 4 };
-
-            Array.Resize(ref arr2, 5);
-
-            for (int i = 0; i < arr2.Length; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
+        static string Sum(int a, int b = 100, string word = "qwerty")
+        {
+            return a + b + word;
         }
     }
 }
