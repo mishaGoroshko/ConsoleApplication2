b6c5f190f2fdd2bc1508cd51d16a01fdceb08c84
MishaGoroshko <mihagoroh@gmail.com>
2023-04-05 11:22:06 +0200

    constructor

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,17 +8,10 @@ namespace ConsoleApplication2
         public static void Main(string[] args)
         {
             Car audi = new Car();
-            audi.Name = "SQ8";
-            audi.Power = 450;
-            audi.Age = 3;
+            // audi.Name = "SQ8";
+            // audi.Power = 450;
+            // audi.Age = 3;
 
-            Console.WriteLine(audi.Name);
-            Console.WriteLine("--------");
-            audi.ShowParams();
-            Console.WriteLine("--------");
-            // audi.ChangeQuality(5, 25);
-            audi.ChangeQualityThis(5, 25);
-            Console.WriteLine("--------");
             audi.ShowParams();
         }
 
@@ -28,6 +21,17 @@ namespace ConsoleApplication2
             public int Power;
             public int Age;
             private readonly int _vin = 82347123;
+
+            public Car(string name, int power, int age)
+            {
+                Name = name;
+                Power = power;
+                Age = age;
+            }
+
+            public Car()
+            {
+            }
 
 
             public void ShowParams()
@@ -43,6 +47,7 @@ namespace ConsoleApplication2
                 Age += lostAges;
                 Power -= lostHorses;
             }
+
             public void ChangeQualityThis(int Age, int lostHorses)
             {
                 this.Age += Age;
